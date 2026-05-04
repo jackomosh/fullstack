ce360140daf59a90aa4382cd7c3fedf9a2051dbd
jackomosh <jackomosh6@gmail.com>
2026-05-04 10:53:04 +0300

    strings.Title: Converts all First Words of a String to UpperCase

---
+++ strings/titles.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	name := "z01 kisumu lake hub"
+	fmt.Println(name)
+	fmt.Println(strings.Title(name)) // converts all first words of a string to UppercCase
+}
