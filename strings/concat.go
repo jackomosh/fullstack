d6413686f5cd755ef7112d9b72357f8890d85076
jacomondi <jackomosh6@gmail.com>
2026-05-16 20:06:06 +0300

    Fmt: looked at %f in fmt

--- strings/concat.go
+++ strings/concat.go
@@ -7,4 +7,5 @@ func main() {
 	wordTwo := "World!"
 	concat := wordOne + " " + wordTwo
 	fmt.Println(concat)
+	fmt.Println(len(concat))
 }
---
+++ strings/float.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var pi float64 = 3.14125
+	fmt.Printf("%.1f \n", pi)
+}
